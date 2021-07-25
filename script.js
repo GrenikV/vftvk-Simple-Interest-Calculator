acf8aeddf32aa199f6d0f7453a1948c36e7ab8fe
GrenikV <87620162+GrenikV@users.noreply.github.com>
2021-07-25 13:08:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function compute()
     {
         //Things that are
+        var p = document.getElementById("principal");
         var principal = document.getElementById("principal").value;
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear()+parseInt(years);
-        p = document.getElementById("principal").value;
 
         // Things that happen
         if (principal <= 0)
